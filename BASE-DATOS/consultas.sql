602fc00eaf4c38390ee54529ff12f6fbafa65c32
Guillermo Duran <guilleducor@gmail.com>
2026-04-04 19:38:32 +0200

    otro

--- BASE-DATOS/consultas.sql
+++ BASE-DATOS/consultas.sql
@@ -15,3 +15,10 @@ SELECT tipo_equipo, ip_estatica
 FROM Hardware
 WHERE tipo_equipo LIKE '%Servidor%' OR tipo_equipo LIKE '%PC%'
 AND ip_estatica LIKE '192.168.60.%';
+
+-- Seguimiento de Bugs Críticos (Filtros y JOIN)
+
+SELECT t.descripcion_bug, t.estado, p.titulo_juegos
+FROM Tickets t
+JOIN Proyectos p ON t.id_proy = p.id_proy
+WHERE t.estado != 'Resuelto';
