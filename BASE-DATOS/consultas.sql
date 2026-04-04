e3197f217496cfa554c773f7ea71c17b224a5cfe
Guillermo Duran <guilleducor@gmail.com>
2026-04-04 19:37:41 +0200

    inventario

--- BASE-DATOS/consultas.sql
+++ BASE-DATOS/consultas.sql
@@ -8,3 +8,10 @@ d.vlan_asociada
 FROM Empleados e
 JOIN Departamentos d ON e.id_dept = d.id_dept
 ORDER BY d.vlan_asociada;
+
+-- Inventario de Hardware por Red (Filtros y Búsqueda)
+
+SELECT tipo_equipo, ip_estatica
+FROM Hardware
+WHERE tipo_equipo LIKE '%Servidor%' OR tipo_equipo LIKE '%PC%'
+AND ip_estatica LIKE '192.168.60.%';
